c1f2d5ff98e6f17eb766c74a401c08407e3aaa52
William Sedlacek <wsedlacekc@gmail.com>
2020-02-11 13:56:20 -0600

    Replace unneeded members with locals
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/controller.py
+++ src/controller.py
@@ -1,43 +1,33 @@
 class Controller:
     _player = None
-    _prompt_question = '\nWhat will you do?'
-    _prompt_quit = '\n  q - Quit'
-    _prompt_inventory = '\n  i - Inventory'
-    _prompt_north = '\n  n - Move North'
-    _prompt_south = '\n  s - Move South'
-    _prompt_west = '\n  w - Move West'
-    _prompt_east = '\n  e - Move East'
-    _prompt_take = '\n  take <item> - Take a item from this room'
-    _prompt_drop = '\n  drop <item> - Drop a item in this room'
-    _prompt = '\n\nCommand: '
 
     def __init__(self, player):
         self._player = player
 
     def _build_prompt(self):
-        prompt = self._prompt_question
-        prompt += self._prompt_quit
-        prompt += self._prompt_inventory
+        prompt = '\nWhat will you do?'
+        prompt += '\n  q - Quit'
+        prompt += '\n  i - Inventory'
 
         if self._player.look_north() is not None:
-            prompt += self._prompt_north
+            prompt += '\n  n - Move North'
 
         if self._player.look_south() is not None:
-            prompt += self._prompt_south
+            prompt += '\n  s - Move South'
 
         if self._player.look_east() is not None:
-            prompt += self._prompt_east
+            prompt += '\n  e - Move East'
 
         if self._player.look_west() is not None:
-            prompt += self._prompt_west
+            prompt += '\n  w - Move West'
 
         if self._player.item_search() is not None:
-            prompt += self._prompt_take
+            prompt += '\n  take <item> - Take a item from this room'
 
         if self._player.bag_search() is not None:
-            prompt += self._prompt_drop
+            prompt += '\n  drop <item> - Drop a item in this room'
 
-        prompt += self._prompt
+        prompt += '\n\nCommand: '
         return prompt
 
     def tick(self):
