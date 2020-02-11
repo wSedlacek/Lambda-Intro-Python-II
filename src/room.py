594460dcd46639f176f23aac66486a86188847da
William Sedlacek <wsedlacekc@gmail.com>
2020-02-11 11:55:26 -0600

    Setup Room
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/room.py
+++ src/room.py
@@ -1,2 +1,32 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+class Room:
+    name = None
+    description = None
+
+    north = None
+    south = None
+    east = None
+    west = None
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f'''==={self.name}===
+{self.description}'''
+
+    def north_of(self, room):
+        room.north = self
+        self.south = room
+
+    def south_of(self, room):
+        room.south = self
+        self.north = room
+
+    def east_of(self, room):
+        room.east = self
+        self.west = room
+
+    def west_of(self, room):
+        room.west = self
+        self.east = room
